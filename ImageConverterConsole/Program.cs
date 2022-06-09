1ec9e5f93e5ef1ec151aa81445b7ea522c462618
Ismael Almaguer <ismaelalmaguer@Ismaels-MacBook-Air.local>
2022-06-08 22:20:10 -0500

    added readme file, added .DS_Store file to gitignore, replaced var instances in console application to make source easier to read

--- ImageConverterConsole/Program.cs
+++ ImageConverterConsole/Program.cs
@@ -18,34 +18,34 @@ catch (Exception ex)
 
 ImageConverterSettings GetSettingsFromArguments()
 {
-    var sourceFilePathIndex = Array.FindIndex(args, a => a == "--source");
+    int sourceFilePathIndex = Array.FindIndex(args, a => a == "--source");
     if (sourceFilePathIndex == -1)
     {
         throw new Exception("Source file path not found.");
     }
-    var sourceFilePath = args[sourceFilePathIndex + 1];
+    string sourceFilePath = args[sourceFilePathIndex + 1];
 
-    var destinationFileTypeIndex = Array.FindIndex(args, a => a == "--dest-type");
+    int destinationFileTypeIndex = Array.FindIndex(args, a => a == "--dest-type");
     if (destinationFileTypeIndex == -1)
     {
         throw new Exception("You did not specify a destination file type.");
     }
-    var destinationFileType = args[destinationFileTypeIndex + 1];
+    string destinationFileType = args[destinationFileTypeIndex + 1];
 
-    var destinationFilePathIndex = Array.FindIndex(args, a => a == "--dest");
-    var destinationFilePath =
+    int destinationFilePathIndex = Array.FindIndex(args, a => a == "--dest");
+    string destinationFilePath =
         destinationFilePathIndex == -1 ?
-        Path.Combine(Path.GetDirectoryName(sourceFilePath) ?? "", Path.GetFileNameWithoutExtension(sourceFilePath)) + "." + destinationFileType :
-        args[destinationFilePathIndex + 1];
+            Path.Combine(Path.GetDirectoryName(sourceFilePath) ?? "", Path.GetFileNameWithoutExtension(sourceFilePath)) + "." + destinationFileType :
+            args[destinationFilePathIndex + 1];
 
-    var destinationWidthIndex = Array.FindIndex(args, a => a == "--dest-width");
-    var destinationWidth = destinationWidthIndex == -1 ? (int?)null : int.Parse(args[destinationWidthIndex + 1]);
+    int destinationWidthIndex = Array.FindIndex(args, a => a == "--dest-width");
+    int? destinationWidth = destinationWidthIndex == -1 ? null : int.Parse(args[destinationWidthIndex + 1]);
 
-    var destinationHeightIndex = Array.FindIndex(args, a => a == "--dest-height");
-    var destinationHeight = destinationHeightIndex == -1 ? (int?)null : int.Parse(args[destinationHeightIndex + 1]);
+    int destinationHeightIndex = Array.FindIndex(args, a => a == "--dest-height");
+    int? destinationHeight = destinationHeightIndex == -1 ? null : int.Parse(args[destinationHeightIndex + 1]);
 
-    var uncompressedIndex = Array.FindIndex(args, a => a == "--uncompressed");
-    var uncompressed = uncompressedIndex != -1;
+    int uncompressedIndex = Array.FindIndex(args, a => a == "--uncompressed");
+    bool uncompressed = uncompressedIndex != -1;
 
     return new ImageConverterSettings(sourceFilePath, destinationFilePath, destinationFileType)
     {
---
+++ README.md
@@ -0,0 +1,70 @@
+# ImageConverter
+
+A series of .NET 6 image converter applications that use a common .NET Standard 2.0 library. All applications allow you to convert an image from one format to another in your desired width and height (unless it is impossible). Uses the [SixLabors.ImageSharp](https://github.com/SixLabors/ImageSharp) library. 
+
+Applications
+
+| Name                      | Tech Stack               |
+| ------------------------- | ------------------------ |
+| ImageConverterConsole     | .NET 6                   |
+| ImageConverterRazor       | .NET 6, ASP.NET Core MVC |
+
+## ImageConverterConsole
+
+A command line utility. Argument names are prefixed with "--" following the argument value.
+
+### Arguments
+
+#### --source
+
+Required: Yes
+
+File path of the image you want to convert.
+
+#### --dest
+
+Required: No
+
+Destination path of your converted image. If not given then the converted image is saved in the same folder and file name as the source path.
+
+#### --dest-type
+
+Required: Yes
+
+File type of your converted image.
+
+Accepted values:
+
+- jpg
+- png
+- bmp
+- gif
+- tga
+
+#### --dest-width
+
+Required: No
+
+Width in pixels of your converted image. If no value is given then the width of your converted image will be the same as your source image.
+
+#### --dest-height
+
+Required: No
+
+Height in pixels of your converted image. If no value is given then the height of your converted image will be the same as your source image.
+
+#### --uncompressed
+
+Required : No
+
+If this argument is present then the converted image will be uncompressed. You don't need to specify a value (true or false) for this argument.
+
+### Example
+
+Converts an image "before.jpg" to an uncompressed PNG file after.png with the dimensions 400x500.
+
+`ImageConverterConsole.exe --source "C:\Pictures\before.jpg --dest "C:\Pictures\after.png" --dest-type "tga" --dest-width 400 --dest-height 500 --uncompressed`
+
+## ImageConverterRazor
+
+On the image converter page `http://localhost:xxxx/Image/New` upload your image and then enter your desired values in the fields. You must specify the conversion type. When you click on the submit button, you will be prompted to download your converted image.
